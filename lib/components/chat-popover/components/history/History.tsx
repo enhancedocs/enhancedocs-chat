f80a1117d07490d468da6e384e78eaf11aae6d81
playolaizq <pol944li@gmail.com>
2023-05-08 08:27:18 +0200

    feat(#8): answer feedback ux/ui

--- lib/components/chat-popover/components/history/History.tsx
+++ lib/components/chat-popover/components/history/History.tsx
@@ -1,5 +1,8 @@
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense, useState } from 'react';
 import { classNames } from '../../../../helpers/styles';
+import CheckCircleIcon from '../../../icons/CheckCircleIcon';
+import ThumbDownIcon from '../../../icons/ThumbDownIcon';
+import ThumbUpIcon from '../../../icons/ThumbUpIcon';
 import type { HistoryItem } from '../../ChatPopover';
 import classes from './History.module.css';
 
@@ -7,21 +10,34 @@ const ReactMarkdown = lazy(() => import('react-markdown'));
 
 type HistoryProps = {
   history: Array<HistoryItem>;
+  onFeedback: ({ answerId, usefulFeedback }: { answerId: string, usefulFeedback: boolean }) => void;
+  loadingAnswer?: boolean;
 }
 
-export default function History ({ history }: HistoryProps)  {
+export default function History ({ history, onFeedback, loadingAnswer }: HistoryProps)  {
+  const [submittedFeedback, setSubmittedFeedback] = useState<{ [key: string]: boolean }>({});
+
+  function handleFeedback ({ answerId, usefulFeedback }: { answerId: string, usefulFeedback: boolean }) {
+    setSubmittedFeedback((prevSubmittedFeedback) => ({ ...prevSubmittedFeedback, [answerId]: true }));
+    onFeedback({ answerId, usefulFeedback });
+  }
+
   return (
     <div className={classes.EnhancedChat__ChatPopover__HistoryContainer}>
-      {history.map((historyItem) => {
+      {history.map((historyItem, index) => {
+        const isFirst = index == 0;
+        const isLast = index == history.length - 1;
+        const isProcessing = loadingAnswer && isLast;
+
         return (
           <div
             className={
               classNames(
                 classes.EnhancedChat__ChatPopover__HistoryItem,
-                historyItem.origin === 'human' && classes.EnhancedChat__ChatPopover__HistoryItemHuman
+                historyItem.origin == 'human' && classes.EnhancedChat__ChatPopover__HistoryItemHuman
               )
             }
-            key={historyItem._id}
+            key={historyItem.answerId}
           >
             <Suspense fallback={<></>}>
               <ReactMarkdown
@@ -40,6 +56,40 @@ export default function History ({ history }: HistoryProps)  {
                 {historyItem.value}
               </ReactMarkdown>
             </Suspense>
+            {
+              historyItem.origin == 'ai' && !isFirst && !isProcessing && (
+                submittedFeedback[historyItem.answerId]
+                  ? (
+                    <div
+                      className={
+                        classNames(
+                          classes.EnhancedChat__ChatPopover__FeedbackContainer,
+                          classes.EnhancedSearch__SearchModal__FeedbackSuccess
+                        )
+                      }
+                    >
+                      <CheckCircleIcon />
+                      <span>Thanks for submitting your feedback!</span>
+                    </div>
+                  )
+                  : (
+                    <div className={classes.EnhancedChat__ChatPopover__FeedbackContainer}>
+                      <div
+                        className={classes.EnhancedChat__ChatPopover__FeedbackButton}
+                        onClick={() => handleFeedback({ answerId: historyItem.answerId, usefulFeedback: true })}
+                      >
+                        <ThumbUpIcon />
+                      </div>
+                      <div
+                        className={classes.EnhancedChat__ChatPopover__FeedbackButton}
+                        onClick={() => handleFeedback({ answerId: historyItem.answerId, usefulFeedback: false })}
+                      >
+                        <ThumbDownIcon />
+                      </div>
+                    </div>
+                  )
+              )
+            }
           </div>
         );
       })}
